75e604d45b5535cdf4c457d4053290484ccc5d73
a-farid <fahizoune10@gmail.com>
2024-05-15 00:12:59 +0100

    test

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,9 +1,9 @@
-import sha1 from 'sha1';
-import Queue from 'bull';
-import { findUserById, findUserIdByToken } from '../utils/helpers';
-import dbClient from '../utils/db';
+import sha1 from "sha1";
+import Queue from "bull";
+import { findUserById, findUserIdByToken } from "../utils/helpers";
+import dbClient from "../utils/db";
 
-const userQueue = new Queue('userQueue');
+const userQueue = new Queue("userQueue");
 
 class UsersController {
   /**
@@ -13,23 +13,26 @@ class UsersController {
     const { email, password } = request.body;
 
     // check for email and password
-    if (!email) return response.status(400).send({ error: 'Missing email' });
-    if (!password) return response.status(400).send({ error: 'Missing password' });
+    if (!email) return response.status(400).send({ error: "Missing email" });
+    if (!password)
+      return response.status(400).send({ error: "Missing password" });
 
     // check if the email already exists in DB
     const emailExists = await dbClient.users.findOne({ email });
-    if (emailExists) return response.status(400).send({ error: 'Already exist' });
+    if (emailExists)
+      return response.status(400).send({ error: "Already exist" });
 
     // Insert new user
     const sha1Password = sha1(password);
     let result;
     try {
       result = await dbClient.users.insertOne({
-        email, password: sha1Password,
+        email,
+        password: sha1Password,
       });
     } catch (err) {
       await userQueue.add({});
-      return response.status(500).send({ error: 'Error creating user' });
+      return response.status(500).send({ error: "Error creating user" });
     }
 
     const user = {
@@ -48,16 +51,18 @@ class UsersController {
    * Should retrieve the user base on the token used
    */
   static async getMe(request, response) {
-    const token = request.headers['x-token'];
-    if (!token) { return response.status(401).json({ error: 'Unauthorized' }); }
+    const token = request.headers["x-token"];
+    if (!token) {
+      return response.status(401).json({ error: "Unauthorized" });
+    }
 
     // Retrieve the user based on the token
     const userId = await findUserIdByToken(request);
-    if (!userId) return response.status(401).send({ error: 'Unauthorized' });
+    if (!userId) return response.status(401).send({ error: "Unauthorized" });
 
     const user = await findUserById(userId);
 
-    if (!user) return response.status(401).send({ error: 'Unauthorized' });
+    if (!user) return response.status(401).send({ error: "Unauthorized" });
 
     const processedUser = { id: user._id, ...user };
     delete processedUser._id;
